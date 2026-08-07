e6367f525d88a8b7b893e42a46724b0db3607dd2
mostafaabdo2274-png <mostafaabdo2274@gmail.com>
2026-08-08 02:24:35 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,10 @@
-
 import os
-
 # Telegram
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-
 # عدد النتائج الافتراضي
 DEFAULT_RESULTS = 50
-
 # User-Agent للطلبات
 USER_AGENT = "LeadHunterBot/1.0"
-
 # الدول المدعومة (ممكن نزودها بعدين)
 COUNTRIES = {
     "egypt": "eg",
