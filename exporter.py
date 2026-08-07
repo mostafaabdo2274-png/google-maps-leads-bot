b4a1beb4f9acf78d111510576bfe24ea6b5c0063
mostafaabdo2274-png <mostafaabdo2274@gmail.com>
2026-08-08 02:34:08 +0300

    Update exporter.py

--- exporter.py
+++ exporter.py
@@ -1,12 +1,16 @@
-
 import csv
 
+
 def export_to_csv(results, filename="results.csv"):
+
     with open(filename, "w", newline="", encoding="utf-8-sig") as file:
+
         writer = csv.writer(file)
 
         writer.writerow([
             "Name",
+            "Phone",
+            "Website",
             "Address",
             "Latitude",
             "Longitude",
@@ -14,8 +18,11 @@ def export_to_csv(results, filename="results.csv"):
         ])
 
         for row in results:
+
             writer.writerow([
                 row.get("name", ""),
+                row.get("phone", ""),
+                row.get("website", ""),
                 row.get("address", ""),
                 row.get("lat", ""),
                 row.get("lon", ""),
